313559f6b536bc81684ea7179a062806535f2ed6
Faith Ward <faithward24@gmail.com>
2023-03-03 16:40:12 -0500

    testing

--- src/Game.java
+++ src/Game.java
@@ -3,7 +3,7 @@ public class Game {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World!");
+		System.out.println("I'm going to make a game!");
 	}
 
 }
